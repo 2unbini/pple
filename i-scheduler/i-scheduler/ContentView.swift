e8e96e0901805f224c30cd26f0c82ddfc7a382ad
Eunbin Kwon <75126613+2unbini@users.noreply.github.com>
2021-12-06 14:31:40 +0900

    [feat] iOS project file #1 (#2)

---
+++ i-scheduler/i-scheduler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  i-scheduler
+//
+//  Created by 권은빈 on 2021/12/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
