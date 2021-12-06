20c1a8546957d31d4f579b698c5e4f25fc868e0a
Eunbin Kwon <kwoneunbini@gmail.com>
2021-12-06 14:24:33 +0900

    [feat] iOS project file #1

---
+++ i-scheduler/i-scheduler/i_schedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  i_schedulerApp.swift
+//  i-scheduler
+//
+//  Created by 권은빈 on 2021/12/06.
+//
+
+import SwiftUI
+
+@main
+struct i_schedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
