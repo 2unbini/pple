fa7f0d4bdce7b2a82307b9df381627730ecc6662
sun <skkimeo@gmail.com>
2021-12-14 16:04:10 +0900

    [feat] integrated core data into PopUpTaskList, TaskList, TaskRowView, TaskDetailView #23

---
+++ i-scheduler/i-scheduler/CheckBox.swift
@@ -0,0 +1,55 @@
+//
+//  Checkbox.swift
+//  i-scheduler
+//
+//  Created by sun on 2021/12/13.
+//
+
+import SwiftUI
+
+struct CheckBox: View {
+    @Environment(\.managedObjectContext) var context
+    @ObservedObject var task: Task
+
+    var body: some View {
+        Button {
+            // maybe take this to the extension
+            // make sth like func publish and put it in all the computed properties
+            task.isFinished.toggle()
+//          replace this with
+//          PersistenceController.shared.saveContext()
+            save()
+        } label: {
+            Image(systemName: task.isFinished ? "checkmark.square" : "square")
+                .scaleEffect(1.3)
+        }
+        .buttonStyle(.plain)
+    }
+    
+    private func save() {
+        task.objectWillChange.send()
+        task.project.objectWillChange.send()
+        do {
+            try context.save()
+        } catch(let error) {
+            print("태스크 저장에 실패했습니다: \(error.localizedDescription)")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//struct Checkbox_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Checkbox()
+//    }
+//}
---
+++ i-scheduler/i-scheduler/TaskList.swift
@@ -0,0 +1,118 @@
+//
+//  TaskList.swift
+//  i-scheduler
+//
+//  Created by sun on 2021/12/07.
+//
+
+import SwiftUI
+
+struct TaskList: View {
+    @Environment(\.managedObjectContext) var context
+    @FetchRequest var tasks: FetchedResults<Task>
+    @Binding var isPresented: Bool
+    var projectId: UUID
+    
+    init(isPresented: Binding<Bool>, projectId: UUID, date: Date) {
+        self._isPresented = isPresented
+        self.projectId = projectId
+        let datePredicates = date.convertedToPredicates()
+        let request = Task.fetchRequest(
+            NSPredicate(
+                format: "project_.projectId_ = %@ and startDate_ < %@ and endDate_ >= %@",
+                argumentArray: [projectId, datePredicates.start, datePredicates.end]
+        ))
+        _tasks = FetchRequest(fetchRequest: request)
+    }
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                if !tasks.isEmpty {
+                    taskList
+                } else {
+                    Text("오늘은 자유!")
+                }
+            }
+            .navigationBarTitle("오늘의 할 일")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) { cancel }
+                ToolbarItem(placement: .navigationBarTrailing) { add }
+            }
+            .sheet(isPresented: $editorIsPresented) {
+                // integrate Eunbin's Editor lator
+            }
+        }
+    }
+    
+    private var taskList: some View {
+        List {
+            ForEach(tasks) { task in
+                TaskRowView(task: task)
+            }
+            .onDelete(perform: deleteTask(at:))
+        }
+    }
+    
+    private func deleteTask(at indexSet: IndexSet) {
+        for index in indexSet {
+            if let task = Task.withId(tasks[index].taskId, context: context) {
+                task.objectWillChange.send()
+                task.project.objectWillChange.send()
+                context.delete(task)
+            }
+        }
+        // replace with PersistenceController.shared.save()
+        do {
+            try context.save()
+        } catch(let error) {
+            print("태스크 삭제에 실패했습니다: \(error.localizedDescription)")
+        }
+    }
+    
+    private var cancel: some View {
+        Button("취소") {
+            withAnimation {
+                isPresented = false
+            }
+        }
+    }
+    
+    @State private var editorIsPresented = false
+
+    private var add: some View {
+        Button {
+            editorIsPresented = true
+        } label: {
+            Image(systemName: "plus")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+//struct DailyTaskList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DailyTaskList()
+//    }
+//}
+
+
+//struct DailyTaskList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TaskList()
+//    }
+//}
