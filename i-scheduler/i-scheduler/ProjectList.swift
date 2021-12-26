9450a8d14b817393a9dff37da9df37a37c60e4bc
Eunbin Kwon <kwoneunbini@gmail.com>
2021-12-27 01:48:11 +0900

    [feat] Fix EditMode Separation in NavigationLink #43

--- i-scheduler/i-scheduler/ProjectList.swift
+++ i-scheduler/i-scheduler/ProjectList.swift
@@ -12,69 +12,58 @@ import CoreData
 struct ProjectList: View {
     
     @Environment(\.managedObjectContext) private var viewContext: NSManagedObjectContext
-    @Environment(\.editMode) var editMode: Binding<EditMode>?
     @FetchRequest(
         entity: Project.entity(),
         sortDescriptors: [NSSortDescriptor(key: "name_", ascending: true)]
     ) var projects: FetchedResults<Project>
     
     @State private var isEditing: Bool = false
-    @State private var isTapped: Bool = false
     @State private var showAddSheet: Bool = false
     @State private var project: Project?
-    
+    @State private var editMode: EditMode = .inactive
     
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    ForEach(projects, id: \.self) { project in
-                        NavigationLink(destination: ProjectCalendarView(project: project),
-                                       label: {
-                            Text(project.name)
-                            // TODO: need to find the way to update isFinished when endDate expires
-                            // stikethrough and foregroundColor change when isFinished
-                                .strikethrough(project.isFinished, color: .gray)
-                                .foregroundColor(project.isFinished ? .gray : .black)
-                                .font(.title3)
-                                .padding(8.0)
-                        })
+            List {
+                ForEach(projects, id: \.self) { project in
+                    if isEditing {
+                        ProjectLabel(project: project)
                             .onTapGesture {
-                                if self.editMode?.wrappedValue == .active {
-                                    self.project = project
-                                    isTapped.toggle()
-                                }
+                                self.project = project
                             }
                     }
-                    .onDelete(perform: removeSelectedProject)
-                }
-                .listStyle(.plain)
-                .navigationTitle(isEditing ? "프로젝트 선택" : "내 프로젝트")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button(isEditing ? "완료" : "수정") {
-                            withAnimation {
-                                self.editMode?.wrappedValue.toggle()
-                                self.isEditing.toggle()
-                            }
-                        }
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        if !isEditing {
-                            Button("추가") {
-                                showAddSheet.toggle()
-                            }
-                            .sheet(isPresented: $showAddSheet, content: {
-                                ProjectAddSheet()
-                            })
+                    else {
+                        NavigationLink(destination: ProjectCalendarView(project: project)) {
+                            ProjectLabel(project: project)
                         }
                     }
                 }
-                .sheet(item: $project) { project in
-                    ProjectEditSheet(editWith: project)
-                }
-                .environment(\.editMode, editMode)
+                .onDelete(perform: removeSelectedProject)
             }
+            .listStyle(.plain)
+            .navigationTitle(isEditing ? "프로젝트 선택" : "내 프로젝트")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(isEditing ? "완료" : "편집") {
+                        editMode.toggle()
+                        isEditing.toggle()
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    if !isEditing {
+                        Button("추가") {
+                            showAddSheet.toggle()
+                        }
+                        .sheet(isPresented: $showAddSheet, content: {
+                            ProjectAddSheet()
+                        })
+                    }
+                }
+            }
+            .sheet(item: $project) { project in
+                ProjectEditSheet(editWith: project)
+            }
+            .environment(\.editMode, $editMode)
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
@@ -88,6 +77,20 @@ struct ProjectList: View {
         } catch {
             fatalError("Cannot save context")
         }
+    }
+}
+
+struct ProjectLabel: View {
+    var project: Project
+    
+    var body: some View {
+        Text(project.name)
+        // TODO: need to find the way to update isFinished when endDate expires
+        // stikethrough and foregroundColor change when isFinished
+            .strikethrough(project.isFinished, color: .gray)
+            .foregroundColor(project.isFinished ? .gray : .black)
+            .font(.title3)
+            .padding(8.0)
     }
 }
 
