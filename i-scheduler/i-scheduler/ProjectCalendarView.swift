50044fc2597d8c62f901ed99866ccb98ed62fd27
tekim-526 <81205931+tekim-526@users.noreply.github.com>
2021-12-31 13:46:35 +0900

    [feat]refactoring_ProjectCalendarView #86 (#86)
    * [feat]refactoring_ProjectCalendarView
    
    * [feat]refactoring_ProjectCalendarView #86

--- i-scheduler/i-scheduler/ProjectCalendarView.swift
+++ i-scheduler/i-scheduler/ProjectCalendarView.swift
@@ -7,70 +7,13 @@
 
 import SwiftUI
 
-struct NavigationTrailingEditButton: View {
-    @State var showModifyTab: Bool = false
-    @State var project: Project
-    
-    var body: some View {
-        Button {
-            self.showModifyTab = !self.showModifyTab
-        } label: {
-            Text("수정")
-        }
-        .sheet(isPresented: $showModifyTab) {
-            ProjectEditSheet(editWith: project)
-        }
-    }
-}
-
-struct DayButtonView: View {
-    let day: Int
-    let date: Date
-    static let dateformat: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "M.d"
-        return formatter
-    }()
-    
-    // MARK: colorOpacity - TaskList에서 할일의 개수를 받아와서 사용 예정
-    let colorOpacity: Double = 20
-
-    let width: CGFloat = UIScreen.main.bounds.size.width
-    let height: CGFloat = UIScreen.main.bounds.size.height
-    var body: some View {
-        ZStack {
-            Image(systemName: "square.fill")
-                .resizable()
-                .frame(width: UIDevice.current.userInterfaceIdiom != .pad ? width / 7 : width / 10,
-                       height: UIDevice.current.userInterfaceIdiom != .pad ? height / 12 : height / 11)
-                .foregroundColor(colorOpacity == 20 ? .white : Color(red: 117/255, green: 249/255, blue: 217/255))
-                .opacity(colorOpacity == 20 ? 1 : colorOpacity / 100)
-                .shadow(color: .black, radius: 2)
-            VStack {
-                Text(String(day))
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                    .frame(alignment: .center)
-                    .foregroundColor(date.midnight == Date().midnight ? .accentColor : .black)
-                    .font(UIDevice.current.userInterfaceIdiom != .pad ? .title3 : .title2)
-                    .padding(2.0)
-                Text("\(date, formatter: DayButtonView.dateformat)")
-                    .lineLimit(1)
-                    .font(UIDevice.current.userInterfaceIdiom != .pad ? .footnote : .title3)
-                    .foregroundColor(date.midnight == Date().midnight ? .accentColor : .black)
-            }
-        }
-        .padding(5)
-    }
-}
-
 struct ProjectCalendarView: View {
     let startDate: Date
     let endDate: Date
     let dayData: [Int]
     
     @ObservedObject var project: Project
-    @State private var showModifyView: Bool = false
+    @State private var isPopupPresented: Bool = false
     @State private var currentIndex: Int?
     @State private var carouselIndex: Int = 0
 
@@ -89,7 +32,7 @@ struct ProjectCalendarView: View {
                             currentIndex = day
                             carouselIndex = day
                         } label: {
-                            DayButtonView(day: day + 1, date: plusDays(startDate: startDate, dayOf: day))
+                            DayButtonView(nTHDay: day + 1, displayedDate: plusDays(startDate: startDate, dayOf: day))
                         }
                     }
                 }
@@ -97,15 +40,15 @@ struct ProjectCalendarView: View {
             .onChange(of: currentIndex) { index in
                 if index != nil {
                     withAnimation {
-                        showModifyView.toggle()
+                        isPopupPresented.toggle()
                     }
                     currentIndex = nil
                 }
             }
-            .popup(isPresented: $showModifyView, dragToDismiss: true, closeOnTap: false, closeOnTapOutside: true) {
+            .popup(isPresented: $isPopupPresented, dragToDismiss: true, closeOnTap: false, closeOnTapOutside: true) {
                 ACarousel(dayData, index: $carouselIndex) { index in
                     TaskList(
-                      isPresented: $showModifyView,
+                      isPresented: $isPopupPresented,
                       project: project,
                       date: plusDays(startDate: startDate, dayOf: index)
                     )
@@ -114,28 +57,28 @@ struct ProjectCalendarView: View {
             }
             .padding()
             .navigationBarTitle(project.name)
-            .navigationBarItems(trailing: NavigationTrailingEditButton(project: self.project)
-                                    .disabled(showModifyView)
+            .navigationBarItems(trailing: ProjectCalendarNavigationTrailingEditButton(project: self.project)
+                                    .disabled(isPopupPresented)
             )
         }
     }
 }
 
-func daysBetween(startDate: Date, endDate: Date) -> Int {
+fileprivate func daysBetween(startDate: Date, endDate: Date) -> Int {
     if endDate < startDate {
         return 1
     }
     return Calendar.current.dateComponents([.day], from: removeUnderHour(fromDate: startDate), to: removeUnderHour(fromDate: endDate)).day!
 }
 
-func removeUnderHour(fromDate: Date) -> Date {
+fileprivate func removeUnderHour(fromDate: Date) -> Date {
     guard let date = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month, .day], from: fromDate)) else {
         fatalError("Failed to strip time from Date object")
     }
     return date
 }
 
-func plusDays(startDate: Date, dayOf: Int) -> Date {
+fileprivate func plusDays(startDate: Date, dayOf: Int) -> Date {
     var dateComponent = DateComponents()
     dateComponent.day = dayOf
     return Calendar.current.date(byAdding: dateComponent, to: startDate)!
